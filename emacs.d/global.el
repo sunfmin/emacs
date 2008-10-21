3fa1587814d7eccfcf812a083df2d17fed6523bb
Joshua Peek <josh@joshpeek.com>
2008-10-21 13:19:18 -0500

    a few more mac related changes

--- emacs.d/global.el
+++ emacs.d/global.el
@@ -1,9 +1,16 @@
-(setq make-backup-files nil)
-(setq-default show-trailing-whitespace t)
-(setq-default column-number-mode t)
-
 ; no splash screen
 (setq inhibit-startup-screen t)
 
+; no auto-save files
+(setq make-backup-files nil)
+
+; no bell
+(setq ring-bell-function 'ignore)
+
+; show extra whitespace
+(setq-default show-trailing-whitespace t)
+
 (setq-default tab-width 2)
-;(setq-default indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
+(setq mac-emulate-three-button-mouse nil)
+(prefer-coding-system 'utf-8)
