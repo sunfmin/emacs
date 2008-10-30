94d5e68863d04799a64657dd0238d1602d180ed6
Joshua Peek <josh@joshpeek.com>
2008-10-30 18:56:43 -0500

    try to recreate all hallows eve and railscasts textmate themes

--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -1,5 +1,8 @@
+(load "themes/all_hallows_eve")
+(load "themes/railscasts")
+
 (require 'color-theme)
 (setq color-theme-is-global t)
-(color-theme-dark-laptop)
+(color-theme-all-hallows-eve)
 
 (setq mac-allow-anti-aliasing nil)
