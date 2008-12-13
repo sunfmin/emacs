50faa48ff4e9e31658b2ce6d83fc2fed25b3e178
Joshua Peek <josh@joshpeek.com>
2008-12-13 00:13:49 -0600

    ruby electric mode

--- emacs.d/modes/ruby.el
+++ emacs.d/modes/ruby.el
@@ -1,4 +1,6 @@
-(autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
+(require 'ruby-mode)
+(require 'ruby-electric)
+
 (setq auto-mode-alist (cons '(".rb$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '(".erb$" . html-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
