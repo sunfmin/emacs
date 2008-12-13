12bffb49fac4d4826239600612db52f1d78fd5fb
Joshua Peek <josh@joshpeek.com>
2008-12-12 18:55:11 -0600

    whitespace

--- emacs.d/defuns.el
+++ emacs.d/defuns.el
@@ -1,25 +1,25 @@
-                                        ; for loading libraries in from the vendor directory
+; for loading libraries in from the vendor directory
 (defun vendor (library)
-	(let* ((file (symbol-name library))
-				 (normal (concat "~/.emacs.d/vendor/" file))
-				 (suffix (concat normal ".el")))
-		(cond
-		 ((file-directory-p normal)
-			(add-to-list 'load-path normal)
-			(require library))
-		 ((file-directory-p suffix)
-			(add-to-list 'load-path suffix)
-			(require library))
-		 ((file-exists-p suffix)
-			(require library)))))
+  (let* ((file (symbol-name library))
+	 (normal (concat "~/.emacs.d/vendor/" file))
+	 (suffix (concat normal ".el")))
+    (cond
+     ((file-directory-p normal)
+      (add-to-list 'load-path normal)
+      (require library))
+     ((file-directory-p suffix)
+      (add-to-list 'load-path suffix)
+      (require library))
+     ((file-exists-p suffix)
+      (require library)))))
 
 (defun tabify-buffer ()
-	(interactive)
-	(tabify (point-min) (point-max)))
+  (interactive)
+  (tabify (point-min) (point-max)))
 
 (defun untabify-buffer ()
-	(interactive)
-	(untabify (point-min) (point-max)))
+  (interactive)
+  (untabify (point-min) (point-max)))
 
 (defun indent-buffer ()
   (interactive)
