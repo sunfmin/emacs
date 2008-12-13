01277ef1646484ea2fc7c614b4b5eccdea088e80
Joshua Peek <josh@joshpeek.com>
2008-12-12 23:43:30 -0600

    need to load rinari

--- emacs.d/modes/ruby.el
+++ emacs.d/modes/ruby.el
@@ -2,3 +2,5 @@
 (setq auto-mode-alist (cons '(".rb$" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '(".erb$" . html-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
+
+(vendor 'rinari)
