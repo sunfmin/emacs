208567d45e57a531fcf6076fd4b767e23e0d8975
Joshua Peek <josh@joshpeek.com>
2008-12-12 23:15:13 -0600

    fuzzy finder ⌘T

--- emacs.d/bindings.el
+++ emacs.d/bindings.el
@@ -1,6 +1,13 @@
 ; general
 (global-set-key "\C-xg" 'magit-status)
 
+; open fuzzy match with command-T
+(global-set-key (kbd "s-t") 'fuzzy-find-in-project)
+
+; switch buffers like tabs in most OS X apps
+(global-set-key (kbd "s-{") 'previous-buffer)
+(global-set-key (kbd "s-}") 'next-buffer)
+
 ; unbind upcase
 (global-unset-key "\C-x\C-u")
 
