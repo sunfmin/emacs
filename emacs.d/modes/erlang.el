88f4eb091e849fde801e1c4749d319f7df9c1945
Felix Sun <sunfmin@gmail.com>
2008-12-17 17:49:42 +0900

    change erlang path and fix theme

--- emacs.d/modes/erlang.el
+++ emacs.d/modes/erlang.el
@@ -1,5 +1,5 @@
 ; erlang
-(setq load-path (cons "/usr/local/lib/erlang/lib/tools-2.6.2/emacs" load-path))
-(setq erlang-root-dir "/usr/local/lib/erlang")
-(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+(setq load-path (cons "/opt/local/lib/erlang/lib/tools-2.6.2/emacs" load-path))
+(setq erlang-root-dir "/opt/local/lib/erlang")
+(setq exec-path (cons "/opt/local/lib/erlang/bin" exec-path))
 (require 'erlang-start)
--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -8,7 +8,8 @@
     (color-theme-gtk-ide)
   (color-theme-dark-laptop))
 
-(setq mac-allow-anti-aliasing nil)
+; (setq mac-allow-anti-aliasing nil)
 
 ; font
-(setq default-frame-alist '((font . "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-Roman-14-*-*-*-*-*-fontset-auto7")))
+; (setq default-frame-alist '((font . "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-Roman-14-*-*-*-*-*-fontset-auto7")))
+(setq default-frame-alist '((font . "-apple-monaco-medium-r-normal--14-140-72-72-m-140-iso10646-1")))
